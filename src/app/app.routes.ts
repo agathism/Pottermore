c7caff023bc5237721e5e709ba4c31bffe6b902b
agathism <soumare.aicha13@gmail.com>
2025-07-03 12:56:57 +0200

    adding details pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { HouseComponent } from './components/house/house.component';
 import { SpellsComponent } from './components/spells/spells.component';
 import { BooksComponent } from './components/books/books.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { CharacterDetailComponent } from './components/character-detail/character-detail.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { HouseDetailComponent } from './components/house-detail/house-detail.component';
 
 export const routes: Routes = [
     { path: 'app-login', component: LoginComponent},
@@ -13,5 +16,8 @@ export const routes: Routes = [
     { path: 'app-spells', component: SpellsComponent},
     { path: 'app-books', component: BooksComponent},
     { path: 'app-movies', component: MoviesComponent},
+    { path: 'app-character-detail/:id', component: CharacterDetailComponent },
+    { path: 'app-movie/detail/:id', component: MovieDetailComponent},
+    { path: 'app-house-detail/:id', component: HouseDetailComponent },
     { path: '', redirectTo: 'app-home', pathMatch: 'full'}
 ];
--- src/app/components/spells/spells.component.ts
+++ src/app/components/spells/spells.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Spell } from '../../models/spell.interface';
 
 @Component({
   selector: 'app-spells',
@@ -8,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './spells.component.scss'
 })
 export class SpellsComponent {
-
+  private httpClient: HttpClient = inject(HttpClient);
+      spells: Spell[] = [];
+      ngOnInit(): void {
+          this.httpClient.get<Spell[]>('https:potterhead-api.vercel.app/api/spells',
+            { headers: { 'accept': 'application/json' } }
+          ).subscribe({
+            next: (data) => {
+              this.spells = data;
+              console.log(this.spells);
+            }
+          });
+      }
 }
