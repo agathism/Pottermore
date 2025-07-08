e92eea5e448a9d84832b5995db70a530c51a349b
agathism <soumare.aicha13@gmail.com>
2025-07-09 00:19:17 +0200

    last push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,6 @@ import { HouseComponent } from './components/house/house.component';
 import { SpellsComponent } from './components/spells/spells.component';
 import { BooksComponent } from './components/books/books.component';
 import { MoviesComponent } from './components/movies/movies.component';
-import { CharacterDetailComponent } from './components/character-detail/character-detail.component';
-import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
-import { HouseDetailComponent } from './components/house-detail/house-detail.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 
@@ -18,8 +15,5 @@ export const routes: Routes = [
     { path: 'app-spells', component: SpellsComponent},
     { path: 'app-books', component: BooksComponent},
     { path: 'app-movies', component: MoviesComponent},
-    { path: 'app-character-detail/:name', component: CharacterDetailComponent },
-    { path: 'app-movie/detail/:id', component: MovieDetailComponent},
-    { path: 'app-house-detail/:id', component: HouseDetailComponent },
     { path: '', redirectTo: 'app-home', pathMatch: 'full'}
 ];
